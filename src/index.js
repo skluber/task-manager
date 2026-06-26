b75eea9ef23eaf11f4a35575b905ba6c6e0b3cb9
skluber <arodriguezzmartinez@gmail.com>
2026-06-26 17:27:45 +0200

    Remover repositorio embebido

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './css/normalize.css';
+import './css/layout.css';
+import './css/sidebar.css';
+import './css/toolbar.css';
+import './css/task-board.css';
+import './css/task-card.css';
+import './css/task-card-completed.css';
+import './css/modal.css';
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: 'auto', 
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', 
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    watchFiles: ['./index.html'],
+    hot: true, 
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
